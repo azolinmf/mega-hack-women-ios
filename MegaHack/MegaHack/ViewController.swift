2b1b706aea81642b58547e5563ce24092034a6d9
Maria Fernanda Azolin <azolin.mf@gmail.com>
2020-08-31 21:51:19 -0300

    Initial commit

---
+++ MegaHack/MegaHack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MegaHack
+//
+//  Created by Maria Fernanda Azolin on 31/08/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
