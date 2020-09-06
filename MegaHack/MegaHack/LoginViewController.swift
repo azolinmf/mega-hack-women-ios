6845109c401d0c818535c030b1402a9524cacf1a
Maria Fernanda Azolin <azolin.mf@gmail.com>
2020-09-06 15:23:53 -0300

    feat(Summary): adds placeholder text to summary view controller

--- MegaHack/MegaHack/LoginViewController.swift
+++ MegaHack/MegaHack/LoginViewController.swift
@@ -15,8 +15,8 @@ class LoginViewController: UIViewController, GIDSignInDelegate {
     
     @IBOutlet weak var subtitleLabel: UILabel!
     @IBOutlet weak var signInButton: GIDSignInButton!
-    @IBOutlet weak var siwaView: UIView!
-    @IBOutlet weak var anotherLoginButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     var loginString: NSString = "Outras formas de login"
     var loginMutableString = NSMutableAttributedString()
     var newUser = true
@@ -26,9 +26,6 @@ class LoginViewController: UIViewController, GIDSignInDelegate {
         super.viewDidLoad()
         
         labelUI()
-        buttonUI()
-        
-        appleSignInConfig()
         
         googleSignInConfig()
         
@@ -110,118 +107,11 @@ class LoginViewController: UIViewController, GIDSignInDelegate {
         normalString.append(attributedString)
         
         subtitleLabel.attributedText = normalString
+        
+        titleLabel.adjustsFontSizeToFitWidth = true
+        titleLabel.minimumScaleFactor = 0.2
+        subtitleLabel.adjustsFontSizeToFitWidth = true
+        subtitleLabel.minimumScaleFactor = 0.2
     }
-    
-    func buttonUI() {
-        loginMutableString = NSMutableAttributedString(string: loginString as String, attributes: [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 12.0, weight: .semibold)])
-        loginMutableString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.MyTheme.orange, range: NSRange(location:17,length:5))
-        loginMutableString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.MyTheme.darkGray, range: NSRange(location:0,length:16))
-        anotherLoginButton.setAttributedTitle(loginMutableString, for: .normal)
-    }
-}
 
-extension LoginViewController : ASAuthorizationControllerPresentationContextProviding {
-    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
-        // return the current view window
-        return self.view.window!
-    }
 }
-
-extension LoginViewController : ASAuthorizationControllerDelegate {
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        print("authorization error")
-        guard let error = error as? ASAuthorizationError else {
-            return
-        }
-        
-        switch error.code {
-        case .canceled:
-            // user press "cancel" during the login prompt
-            print("Canceled")
-        case .unknown:
-            // user didn't login their Apple ID on the device
-            print("Unknown")
-        case .invalidResponse:
-            // invalid response received from the login
-            print("Invalid Respone")
-        case .notHandled:
-            // authorization request not handled, maybe internet failure during login
-            print("Not handled")
-        case .failed:
-            // authorization failed
-            print("Failed")
-        @unknown default:
-            print("Default")
-        }
-    }
-    
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
-        
-        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
-            let userID = appleIDCredential.user
-            
-            // optional, might be nil
-            let email = appleIDCredential.email
-            
-            // optional, might be nil
-            let givenName = appleIDCredential.fullName?.givenName
-            
-            // optional, might be nil
-            let familyName = appleIDCredential.fullName?.familyName
-            
-            // optional, might be nil
-            let nickName = appleIDCredential.fullName?.nickname
-            
-            /*
-             useful for server side, the app can send identityToken and authorizationCode
-             to the server for verification purpose
-             */
-            var identityToken : String?
-            if let token = appleIDCredential.identityToken {
-                identityToken = String(bytes: token, encoding: .utf8)
-            }
-            
-            var authorizationCode : String?
-            if let code = appleIDCredential.authorizationCode {
-                authorizationCode = String(bytes: code, encoding: .utf8)
-            }
-            
-            // do what you want with the data here
-        }
-    }
-    
-    func appleSignInConfig() {
-        let siwaButton = ASAuthorizationAppleIDButton()
-        siwaButton.translatesAutoresizingMaskIntoConstraints = false
-        siwaView.addSubview(siwaButton)
-        NSLayoutConstraint.activate([
-            siwaButton.leadingAnchor.constraint(equalTo: siwaView.safeAreaLayoutGuide.leadingAnchor, constant: 0.0),
-            siwaButton.trailingAnchor.constraint(equalTo: siwaView.safeAreaLayoutGuide.trailingAnchor, constant: 0.0),
-            siwaButton.widthAnchor.constraint(equalTo: signInButton.safeAreaLayoutGuide.widthAnchor),
-            siwaButton.heightAnchor.constraint(equalTo: signInButton.safeAreaLayoutGuide.heightAnchor)
-        ])
-        siwaButton.addTarget(self, action: #selector(appleSignInTapped), for: .touchUpInside)
-    }
-    
-    @objc func appleSignInTapped() {
-        let provider = ASAuthorizationAppleIDProvider()
-        let request = provider.createRequest()
-        // request full name and email from the user's Apple ID
-        request.requestedScopes = [.fullName, .email]
-        
-        // pass the request to the initializer of the controller
-        let authController = ASAuthorizationController(authorizationRequests: [request])
-        
-        // similar to delegate, this will ask the view controller
-        // which window to present the ASAuthorizationController
-        authController.presentationContextProvider = self
-        
-        // delegate functions will be called when user data is
-        // successfully retrieved or error occured
-        authController.delegate = self
-        
-        // show the Sign-in with Apple dialog
-        authController.performRequests()
-    }
-}
-
--- MegaHack/MegaHack/SummaryViewController.swift
+++ MegaHack/MegaHack/SummaryViewController.swift
@@ -10,21 +10,12 @@ import UIKit
 
 class SummaryViewController: UIViewController {
 
+    @IBOutlet weak var homeName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        homeName.text = Profile.shared.homeName
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
